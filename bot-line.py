6bfb6dfb97c4b1642211619d9b33ffe6dbcea85d
Harit <slipknot.opal@gmail.com>
2018-04-20 09:17:09 +0700

    remove defualt message

--- bot-line.py
+++ bot-line.py
@@ -48,7 +48,7 @@ bot = ChatBot(
         {
             'import_path': 'chatterbot.logic.LowConfidenceAdapter',
             'threshold': 0.90,
-            'default_response': "น้องซอฟไม่เข้าใจ"
+            'default_response': ""
         }
     ],
     input_adapter="chatterbot.input.VariableInputTypeAdapter",
